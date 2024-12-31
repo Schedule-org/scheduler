8ed22c10b3bf76e1a8363d51bc6bf37bc0df3201
hebertsanto <hebertsantosdeveloper@gmail.com>
2024-12-30 21:45:31 -0300

    feat: add loki to visualize logs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,8 +4,10 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/natefinch/lumberjack"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sirupsen/logrus"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
@@ -31,19 +33,36 @@ func handler(c *gin.Context) {
 
 // @title Scheduler app
 // @version 1.0
-// @description Saas plataform
+// @description Saas plataforma
 
-// @contact.name Hebert santos
+// @contact.name Hebert Santos
 // @contact.url https://www.hebertzin.com/
 // @contact.email hebertsantosdeveloper@gmail.com
 
 // @BasePath /api/v1
 func main() {
+
+	logrus.SetOutput(&lumberjack.Logger{
+		Filename:   "/var/log/app.log",
+		MaxSize:    10,
+		MaxBackups: 3,
+		MaxAge:     28,
+		Compress:   true,
+	})
+
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+
 	r := gin.Default()
 	r.GET("/api/v1/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	r.GET("/", handler)
 	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
+
+	logrus.WithFields(logrus.Fields{
+		"status": "success",
+	}).Info("Server is running!!")
 	r.Run(":8080")
 
 }
