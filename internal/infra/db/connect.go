0bf09ef77a701b98df0b80015c2dcd6e3d8d0884
hebertsanto <hebertsantosdeveloper@gmail.com>
2024-12-30 21:56:36 -0300

    chore: conect database method

---
+++ internal/infra/db/connect.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/hebertzin/tadix-backend/internal/domains"
+	"github.com/sirupsen/logrus"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var db *gorm.DB
+
+func ConnectDatabase(cfg *domains.Config) *gorm.DB {
+	dsn := fmt.Sprintf("user=%s password=%s dbname=%s port=%s host=%s",
+		cfg.User, cfg.Password, cfg.Database, cfg.Port, cfg.Host)
+
+	var err error
+
+	gormLogger := logger.New(
+		logrus.New(),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		},
+	)
+
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: gormLogger,
+	})
+	if err != nil {
+		log.Fatalf("Failed to connect to the db: %v", err)
+	}
+
+	fmt.Println("Database connection successfully established")
+
+	return db
+}
