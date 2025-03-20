7bb01791003bea452e27690f26f95caac5c758be
hebertsanto <hebertsantosdeveloper@gmail.com>
2025-03-20 13:47:25 -0300

    docs(availability, controllers): create docs to professional availability controllers

--- internal/presentation/controllers/v1/professional_availability.go
+++ internal/presentation/controllers/v1/professional_availability.go
@@ -23,6 +23,17 @@ func NewProfessionalAvailabilityController(uc domains.ProfessionalsAvailabilityU
 	return &ProfessionalAvailabilityHandler{uc: uc}
 }
 
+// Add godoc
+// @Summary      Add
+// @Description  Add
+// @Tags         ProfessionalAvailability
+// @Accept       json
+// @Produce      json
+// @Param        establishment  body      domains.Establishment  true  "ProfessionalAvailabilityHandler data"
+// @Success      201            {object}  domains.HttpResponse{data=domains.ProfessionalAvailability}  "professional availability created successfully by id successfully retrieved"
+// @Failure      400            {object}  domains.HttpResponse  "Bad Request"
+// @Failure      500            {object}  domains.HttpResponse  "Internal Server Error"
+// @Router       /availability/ [post]
 func (h *ProfessionalAvailabilityHandler) Add(ctx *gin.Context) {
 	var input domains.ProfessionalAvailability
 	if err := ctx.ShouldBindJSON(&input); err != nil {
@@ -39,6 +50,17 @@ func (h *ProfessionalAvailabilityHandler) Add(ctx *gin.Context) {
 	h.RespondWithSuccess(ctx, http.StatusCreated, "professional availability created successfully", availability)
 }
 
+// Add godoc
+// @Summary      Add
+// @Description  Add
+// @Tags         ProfessionalAvailability
+// @Accept       json
+// @Produce      json
+// @Param        establishment  body      domains.Establishment  true  "ProfessionalAvailabilityHandler data"
+// @Success      201            {object}  domains.HttpResponse{data=domains.ProfessionalAvailability}  "professional availability retrieved successfullys availability created successfully by id successfully retrieved"
+// @Failure      400            {object}  domains.HttpResponse  "Bad Request"
+// @Failure      500            {object}  domains.HttpResponse  "Internal Server Error"
+// @Router       /availability/:id/professional [get]
 func (h *ProfessionalAvailabilityHandler) GetProfessionalAvailabilityById(ctx *gin.Context) {
 	professional_id := ctx.Param("id")
 	availability, err := h.uc.GetProfessionalAvailabilityById(ctx.Request.Context(), professional_id)
