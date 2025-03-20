035ee2cd18b79b30893c6dbb94863142f202106c
hebertsanto <hebertsantosdeveloper@gmail.com>
2025-03-20 15:00:12 -0300

    refactor(routes): creating a function to initialize all routes

---
+++ internal/presentation/router/init_routes.go
@@ -0,0 +1,17 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+func InitRoutes(router *gin.Engine, db *gorm.DB, logger *logrus.Logger) error {
+	UsersGroupRouter(router, db, logger)
+	EstablishmentGroupRouter(router, db, logger)
+	ProfessionalsGroupRouter(router, db, logger)
+	ServicesGroupRouter(router, db, logger)
+	AppointmentGroupRouter(router, db, logger)
+	ProfessionalAvailabilityGroupRouter(router, db, logger)
+	ProfessionalAvailabilityGroupRouter(router, db, logger)
+}
