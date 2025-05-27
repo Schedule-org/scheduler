ea48500f6d9e9b0981da7f70ba16ca352f6da56b
hebertsanto <hebertsantosdeveloper@gmail.com>
2025-05-26 21:25:40 -0300

    feat: add interfaces in domain layer

--- internal/domain/account.go
+++ internal/domain/account.go
@@ -1,8 +1,30 @@
 package domain
 
+import (
+	"context"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hebertzin/scheduler/internal/core"
+)
+
 type (
 	Account struct {
 		Email string `json:"email"`
 		Cnpj  string `json:"cnpj"`
+	}
+
+	AccountUseCase interface {
+		// Add creates a new account.
+		Add(ctx context.Context, payload *Account) (*Account, *core.Exception)
+	}
+
+	AccountRepository interface {
+		// Add creates a new account.
+		Add(ctx context.Context, payload *Account) (*Account, *core.Exception)
+	}
+
+	AccountController interface {
+		// Add handles the HTTP request to create a new account.
+		Add(ctx *gin.Context)
 	}
 )
