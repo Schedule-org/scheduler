3887ae4d79e7f790a177952763819523dc809367
Hebert Santos <hebertsantosdeveloper@gmail.com>
2025-06-27 18:23:06 -0300

    refactor: improve code identation

--- internal/presentation/router/accounts.go
+++ internal/presentation/router/accounts.go
@@ -3,7 +3,6 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/hebertzin/scheduler/internal/infra/factory"
-	"github.com/hebertzin/scheduler/internal/presentation/middlewares"
 
 	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
@@ -14,13 +13,8 @@ func StartAccountsApi(router *gin.Engine, db *gorm.DB, logger *logrus.Logger) {
 	v1 := router.Group("/api/v1")
 	{
 		v1.GET("/accounts", accountFactory.FindAllAccounts)
-
 		v1.POST("/accounts", accountFactory.Add)
-
-		v1.Use(middlewares.ValidateParamRequest())
-
 		v1.GET("/accounts/:id", accountFactory.FindAccountById)
-
 		v1.GET("/accounts/:id/establishments", accountFactory.FindAllEstablishmentsByAccountId)
 	}
 }
