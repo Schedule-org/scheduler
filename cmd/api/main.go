bae3799e9882285c90c7c275ed3036840236715c
Hebert Santos <hebertsantosdeveloper@gmail.com>
2025-06-28 20:25:35 -0300

    chore: add graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/hebertzin/scheduler/internal/domain"
 	"github.com/hebertzin/scheduler/internal/infra/config/env"
@@ -34,9 +42,32 @@ func main() {
 	cofigureMetrics(config, r)
 	loggging := configureLogging(config)
 	router.StartApi(r, database, loggging)
-	if err := r.Run(config.Port); err != nil {
-		println("some error has been occurred:", err.Error())
+
+	srv := http.Server{
+		Addr:    config.Port,
+		Handler: r,
 	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Println("Server Shutdown:", err)
+	}
+
+	<-ctx.Done()
+	log.Println("timeout of 5 seconds.")
+	log.Println("Server exiting")
 }
 
 func createRouter() *gin.Engine {
